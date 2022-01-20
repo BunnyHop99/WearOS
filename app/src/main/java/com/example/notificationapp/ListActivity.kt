dc7797e02887dc13dcfcbd70b446b176bdddf788
Andres Sanchez Gomez <77746129+BunnyHop99@users.noreply.github.com>
2022-01-20 02:16:24 -0700

    sharedPreference

---
+++ app/src/main/java/com/example/notificationapp/ListActivity.kt
@@ -0,0 +1,46 @@
+package com.example.notificationapp
+
+import android.app.Activity
+
+import android.os.Bundle
+import com.example.notificationapp.databinding.ActivityListBinding
+
+import android.widget.*
+
+
+
+class ListActivity : Activity() {
+
+    private lateinit var binding: ActivityListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPref = getSharedPreferences("preferences", MODE_PRIVATE)
+
+        val savedTable = sharedPref.getString("keytable", null)
+        val savedKitchen = sharedPref.getString("keykitchen", null)
+        val savedBar = sharedPref.getString("keybar", null)
+
+        val arrayAdapter :ArrayAdapter<*>
+        val notificacionesLista : MutableList<String> = mutableListOf()
+        val lvDatos = findViewById<ListView>(R.id.LvDatos)
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, notificacionesLista)
+        lvDatos.adapter = arrayAdapter
+
+        if (savedTable != null) {
+            notificacionesLista.add(savedTable)
+        }
+        if (savedKitchen != null) {
+            notificacionesLista.add(savedKitchen)
+        }
+        if (savedBar != null) {
+            notificacionesLista.add(savedBar)
+        }
+
+    }
+}
--- app/src/main/java/com/example/notificationapp/MainActivity.kt
+++ app/src/main/java/com/example/notificationapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.notificationapp
 
+import android.annotation.SuppressLint
 import android.app.*
-import android.content.ContentValues.TAG
+
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
-import android.net.Uri
+
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+
+import android.view.GestureDetector
+import android.view.MotionEvent
+
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -17,7 +21,7 @@ import androidx.core.app.NotificationManagerCompat
 import com.example.notificationapp.databinding.ActivityMainBinding
 
 import org.eclipse.paho.android.service.MqttAndroidClient
-import org.eclipse.paho.client.mqttv3.*
+
 import org.eclipse.paho.client.mqttv3.MqttException
 
 import org.eclipse.paho.client.mqttv3.IMqttToken
@@ -27,37 +31,28 @@ import org.eclipse.paho.client.mqttv3.IMqttActionListener
 import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.MqttMessage
-import java.io.UnsupportedEncodingException
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 
-import com.google.android.gms.wearable.MessageEvent
 
 import org.eclipse.paho.client.mqttv3.MqttCallback
 import java.lang.Exception
-import java.util.*
+import java.lang.Math.abs
 
 
-class MainActivity : Activity() {
+
+class MainActivity : Activity(), GestureDetector.OnGestureListener {
 
     private var channelID = "channelID"
     private var channelName = "channelName"
 
     private var notificationID = 10
 
-    private lateinit var mqttClient: MqttAndroidClient
-
-    companion object {
-        const val  INTENT_REQUEST = 0
-        const val BUTTON_INTENT_REQUEST = 1
-    }
-
     //Broker data
     private val MQTTHOST : String = "wss://awesome-engineer.cloudmqtt.com:443/mqtt"
     private val USERNAME : String = "vinnyiht"
     private val PASSWORD : String = "pmH4p_mZ-ae3"
     val topicStr : String = "topic"
-
-
 
     private fun createNotificationChannel() {
 
@@ -69,17 +64,37 @@ class MainActivity : Activity() {
             }
 
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
             manager.createNotificationChannel(channel)
+    }
+
+
+    lateinit var gestureDetector: GestureDetector
+    var x2:Float = 0.0f
+    var x1:Float = 0.0f
+
+    companion object{
+        const val MIN_DISTANCE = 150
     }
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        gestureDetector = GestureDetector(this, this)
+
+        fun guardarNot(){
+            val sharedPreferences : SharedPreferences = getSharedPreferences("preferences", Context.MODE_PRIVATE)
+            val editor : SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("keytable", "Mesa 4")
+            editor.putString("keykitchen", "Cocina")
+            editor.putString("keybar", "Bar")
+            editor.apply()
+        }
 
         val statusText = findViewById<TextView>(R.id.statusText)
 
@@ -110,6 +125,7 @@ class MainActivity : Activity() {
             val token = client.connect(options)
             token.actionCallback = object : IMqttActionListener {
 
+                @SuppressLint("SetTextI18n")
                 override fun onSuccess(asyncActionToken: IMqttToken) {
                     // We are connected
                     setSubscription()
@@ -117,6 +133,7 @@ class MainActivity : Activity() {
                     statusText.setTextColor(Color.parseColor("#00FF0A"))
                 }
 
+                @SuppressLint("SetTextI18n")
                 override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
                     // Something went wrong e.g. connection timeout or firewall problems
                     Toast.makeText(applicationContext,"Connected failed",Toast.LENGTH_LONG).show()
@@ -144,9 +161,8 @@ class MainActivity : Activity() {
         createNotificationChannel()
 
         val notification = NotificationCompat.Builder(this, channelID).also {
-            val resultIntent = Intent(this, ReplyActivity::class.java)
+            val resultIntent = Intent(this, ListActivity::class.java)
             val resultPendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
             it.setSmallIcon(R.drawable.ic_message)
             it.setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
             it.priority = NotificationCompat.PRIORITY_HIGH
@@ -170,11 +186,7 @@ class MainActivity : Activity() {
             @Throws(Exception::class)
             override fun messageArrived(topic: String, message: MqttMessage) {
                 notificationManager.notify(notificationID, notification)
-                /*Timer().scheduleAtFixedRate(object : TimerTask() {
-                    override fun run() {
-
-                    }
-                }, 0, 5000)*/
+                guardarNot()
             }
 
             override fun deliveryComplete(token: IMqttDeliveryToken) {
@@ -182,6 +194,73 @@ class MainActivity : Activity() {
             }
         })
 
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        gestureDetector.onTouchEvent(event)
+
+        when (event?.action){
+
+            0-> {
+                x1 = event.x
+            }
+
+            1-> {
+                x2 = event.x
+
+                val valueX:Float = x2-x1
+
+                if(abs(valueX) > MIN_DISTANCE) {
+                    if (x1 > x2){
+                        val resultIntent = Intent(this, ListActivity::class.java)
+                        startActivity(resultIntent)
+                    }
+                }
+
+            }
+        }
+
+        return super.onTouchEvent(event)
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onScroll(
+        p1: MotionEvent?,
+        p2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onFling(
+        p0: MotionEvent?,
+        p1: MotionEvent?,
+        p2: Float,
+        p3: Float
+    ): Boolean {
+        //TODO("Not yet implemented")
+        return false
     }
 
 }
